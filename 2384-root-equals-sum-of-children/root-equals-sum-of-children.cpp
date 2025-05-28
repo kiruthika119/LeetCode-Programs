c69999158f2064cc540494913288571aa025a120
Kiruthika B <kiruthika.b119@gmail.com>
2025-05-28 09:45:22 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (65.86%) - LeetSync

---
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        int a = root->left->val;
+        int b = root->right->val;
+        int ans = a+b;
+        if(ans == root->val)
+           return true;
+        else
+          return false;
+    }
+};
