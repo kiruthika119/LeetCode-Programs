0451dd88e28ae314626d2aa7e3ce34811cdd55d2
Kiruthika B <kiruthika.b119@gmail.com>
2025-05-28 18:19:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (23.05%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        if(abs (x-z) == abs(y-z))
+          return 0;
+        return abs(x-z) < abs(y-z) ? 1 : 2;
+        
+    }
+};
